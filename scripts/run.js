5964412a905f47365bddc71da4885c28a512b380
vipin rawat <rawat.vipin4818@gmail.com>
2021-09-19 05:57:16 +0530

    added smart contract depolyment script

---
+++ scripts/run.js
@@ -0,0 +1,6 @@
+async function main() {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("Contract deployed to:", waveContract.address);
+}
